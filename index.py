0f6891b74295898ec03adcca0f57af5d7d16e8e0
borrisCodes <johnsonbcameron@gmail.com>
2024-11-14 17:44:18 -0500

    Creates the function defining decoder logic Takes user input of roman numerals and converts it into integer form

---
+++ index.py
@@ -0,0 +1,20 @@
+def romanToInt(s):
+        sum = 0
+        numeral_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for index, item in enumerate(s):
+            next = index + 1
+            number = numeral_values.get(item)
+            if next < len(s):
+                next_number = numeral_values.get(s[next])
+
+                if number == 1 and next_number == 5 or number == 1 and next_number == 10:
+                    number = number - 2
+                elif number == 10 and next_number == 50 or number == 10 and next_number == 100:
+                    number = number - 20
+                elif number == 100 and next_number == 500 or number == 100 and next_number == 1000:
+                    number = number - 200
+            
+            sum = sum + number
+        print(sum)
+
+romanToInt('LVIII')
