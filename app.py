a172eb712fda485c5c921047e00f93c262be0eb9
borrisCodes <johnsonbcameron@gmail.com>
2024-11-26 23:37:43 -0500

    Used Flask to render numeral to integer from input

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from main import romanToInt, rgenNumeral
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def start():
+    if request.method == 'POST':
+        rnumeral = request.form['calc']
+        integer = romanToInt(rnumeral)
+        return render_template('index.html', integer=integer)
+    
     return render_template('index.html')
 
-@app.route('/submit', methods = ['POST'])
-def submit_form():
-    numerals = request.form['calc']
-    return numerals
+
     
 if __name__ == '__main__':
     app.run()
